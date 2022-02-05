9f50622e444f09e6a360ec49b1b3e1041da3edba
Icko9 <99085957+Icko9@users.noreply.github.com>
2022-02-05 16:36:39 +0100

    Prva&VtoraDomasna

---
+++ lab01/Assets/PlayerWalk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+
+    public int playerSpeed;
+    public float start = -30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+       // if(Camera.main.transform.rotation.eulerAngles.x >= -10 && Camera.main.transform.eulerAngles.x < 40)
+        //{
+
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+
+//        }
+      
+    }
+}
